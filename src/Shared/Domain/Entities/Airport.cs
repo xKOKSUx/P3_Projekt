435ee7188a93b56e398101511b8a4eb643cb7f37
Lucifer <SurdackiD1@gmail.com>
2024-10-25 17:53:38 +0200

    Dodanie klas, uporządkowanie projektu

---
+++ src/Shared/Domain/Entities/Airport.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities
+{
+    public class Airport 
+    {
+        public string country { get; set; }
+        public string city { get; set; }
+        public string code { get; set; }
+
+        public Airport()
+        {
+            country = "";
+            city = "";
+            code = "";
+        }
+    }
+}
---
+++ src/Shared/Domain/Entities/Plane.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Entities
+{
+    public class Plane
+    {
+        string Model { get; set; }
+        string Manufacturer { get; set; }
+        string Code { get; set; }
+        string DOA { get; set; }
+        string DOD { get; set; }
+        string Heading { get; set; }
+
+        public Plane()
+        {
+            Model = "";
+            Manufacturer = "";
+            Code = "";
+            DOA = "";
+            DOD = "";
+            Heading = "";
+
+        }
+    }
+}
