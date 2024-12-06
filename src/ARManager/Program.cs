731d13fff1bd44d29cbf34bb31a32cd1fa53e899
Lucifer <SurdackiD1@gmail.com>
2024-12-06 16:46:33 +0100

    Zmieniono ścieżke pliku json

--- src/ARManager/Program.cs
+++ src/ARManager/Program.cs
@@ -4,24 +4,20 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 
-const string path = @"C:\Users\Surda\Documents\P3_Projekt\Planes.json";
-string jsonR = File.ReadAllText(path);
-if (!File.Exists(path))
-{
-    Console.WriteLine($"Error: File '{path}' not found!");
-    return;
-}
+string path = Path.Combine("", @"..\..\..\..\..\Planes.json");
+//string jsonR = File.ReadAllText(path);
+//if (!File.Exists(path))
+//{
+//    Console.WriteLine($"Error: File '{path}' not found!");
+//    return;
+//}
 
-Dictionary<string, Plane> planes = JsonSerializer.Deserialize<Dictionary<string, Plane>>(jsonR);
-
-
-
-
+//Dictionary<string, Plane> planes = JsonSerializer.Deserialize<Dictionary<string, Plane>>(jsonR);
 
 
 Airport airport = new Airport("USA", "Los Angeles", "LAX");
 List<Plane> plane= new List<Plane>();
-//Dictionary<string, Plane> planes = new Dictionary<string, Plane>();
+Dictionary<string, Plane> planes = new Dictionary<string, Plane>();
 
 void createplane()
 {
@@ -78,6 +74,6 @@ void createplane()
 createplane();
 
 
-string json = JsonSerializer.Serialize(planes);
-File.WriteAllText(path, json);
+string jsonW = JsonSerializer.Serialize(planes);
+File.WriteAllText(path, jsonW);
 //PlaneService.DisplayInfoPlane();
