8549ff2f5244f5ce23216d0b0bdc4a91801f6a00
Lucifer <SurdackiD1@gmail.com>
2025-01-10 11:17:33 +0100

    Dodanie zapisu plików w osobnej funkcji.

---
+++ src/ARManager/Services/ImportSaveData.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Json;
+
+namespace ARManager.Services
+{
+    public class ImportSaveData
+    {
+        string path = Path.Combine("", @"..\..\..\..\..\Planes.json");
+       
+
+        public void CreateFile()
+        {
+            try
+            {
+                if (!File.Exists(path))
+                {
+                    File.WriteAllText(path, "[]");
+                    Console.WriteLine("Utworzono plik planes.json.");
+                }
+                else
+                {
+                    Console.WriteLine("Plik planes.json już istnieje.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Wystąpił błąd podczas tworzenia pliku: {ex.Message}");
+            }
+        }
+
+        public void SaveToFile(string path, object data)
+        {
+            string jsonData = JsonSerializer.Serialize(data, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(path, jsonData);
+        }
+
+        public T? ReadFromFile<T>(string path)
+        {
+
+            string jsonData = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<T>(jsonData);
+        }
+
+        public void DeleteFile()
+        {
+
+            if (File.Exists(path)) File.Delete(path);
+            Console.WriteLine("Pliki zostały usunięte.");
+        }
+    }
+}
